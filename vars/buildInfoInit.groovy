134065797f7d4672ff0da470d7377c004f19ed8a
huynhminhchu <34889054+huynhminhchu@users.noreply.github.com>
2022-06-06 11:10:08 +0700

    Update buildInfoInit.groovy

--- vars/buildInfoInit.groovy
+++ vars/buildInfoInit.groovy
@@ -1,5 +1,7 @@
 
 def call(Object[] args){
+    def build_name = args[0].build_name 
+    build_name += env.BRANCH_NAME ? "-${env.BRANCH_NAME}" : ''
     rtBuildInfo (
         captureEnv: true,
         buildName: args[0].build_name,
