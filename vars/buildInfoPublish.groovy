727483267ccf64f003bae4d15defc1db5486a492
huynhminhchu <34889054+huynhminhchu@users.noreply.github.com>
2022-07-06 14:04:13 +0700

    Update buildInfoPublish.groovy

--- vars/buildInfoPublish.groovy
+++ vars/buildInfoPublish.groovy
@@ -6,4 +6,14 @@ def call(Object[] args){
         buildName: args[0].build_name,
         buildNumber: build_number
     )
+    def is_pr = (env.BRANCH_NAME) ? env.BRANCH_NAME.matches("PR-(.*)") : false
+    def build_info_path = "artifactory-build-info/${build_name}/${build_number}-*.json"
+    def build_info_props = ""
+    build_info_props += is_pr  ? "deb.retention_days=7"  : ''
+    rtSetProps (
+      serverId: test-artifactory,
+      spec: "{\"files\":[{\"pattern\":\"${build_info_path}\",\"sortBy\":[\"created\"],\"sortOrder\":\"desc\",\"limit\":1}]}",
+      props: build_info_props,
+      failNoOp: true
+   ) 
 }
